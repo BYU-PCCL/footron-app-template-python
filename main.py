c7dbead26e12adc1c44fd074dbf765eb0166df76
Vin Howe <24789592+vinhowe@users.noreply.github.com>
2021-07-29 18:01:19 -0600

    Clean up and refactor

--- main.py
+++ main.py
@@ -7,12 +7,10 @@ messaging = foomsg.Messaging()
 
 
 def message_handler(message):
-    print("Received message:")
     print(message)
 
 
 async def main_loop():
-    messaging.add_message_listener(message_handler)
     while True:
         await messaging.send_message(round(time.time() * 1000))
         await asyncio.sleep(0.05)
@@ -20,6 +18,7 @@ async def main_loop():
 
 if __name__ == "__main__":
     event_loop = asyncio.get_event_loop()
-    event_loop.create_task(messaging.start())
+    messaging.add_message_listener(message_handler)
+    messaging_loop = event_loop.create_task(messaging.start())
     event_loop.create_task(main_loop())
-    event_loop.run_forever()
+    event_loop.run_until_complete(messaging_loop)
